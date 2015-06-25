a89db5a5bdd08ed84df546dce9175d1b6c602e4f
Alex <Alex@Alex-PC>
2015-06-25 17:04:29 +0200

    migliorata GUI: ora è utilizzabile per test su KNX senza far partire Freedomotic

--- plugins/devices/KNX-over-IP/src/main/java/com/freedomotic/plugins/devices/KNX/KNX.java
+++ plugins/devices/KNX-over-IP/src/main/java/com/freedomotic/plugins/devices/KNX/KNX.java
@@ -10,11 +10,21 @@ package com.freedomotic.plugins.devices.KNX;
 import com.freedomotic.api.EventTemplate;
 import com.freedomotic.api.Protocol;
 import com.freedomotic.exceptions.UnableToExecuteException;
-import com.freedomotic.things.ThingRepository;
 import com.freedomotic.reactions.Command;
 import com.google.inject.Inject;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import tuwien.auto.calimero.GroupAddress;
 import tuwien.auto.calimero.exception.KNXException;
@@ -28,50 +38,44 @@ public class KNX
         extends Protocol {
 
     private static final Logger LOG = Logger.getLogger(KNX.class.getName());
-    final int POLLING_WAIT;
-    final String KNX_IP = "127.0.0.1";
-    final int KNX_PORT = 3671;
-    final String KNX_group = "1/1/1";
+    private String KNX_IP = "127.0.0.1";
+    //private int KNX_PORT = 3671;  porta standard
+    private String KNX_group = "1/1/1";
     private boolean KNX_value = false;
     private KNXNetworkLink knxLink = null;
     private ProcessCommunicator pc = null;
+    private KNXGui settingsMenu = null;
+    private boolean swState = false;
 
     @Inject
-    private ThingRepository thingsRepository;
 
     public KNX() {
-        //every plugin needs a name and a manifest XML file
         super("KNX-over-IP", "/KNX-over-IP/KNX-over-IP-manifest.xml");
-        //read a property from the manifest file below which is in
-        //FREEDOMOTIC_FOLDER/plugins/devices/com.freedomotic.hello/hello-world.xml
-        POLLING_WAIT = configuration.getIntProperty("time-between-reads", 2000);
-        //POLLING_WAIT is the value of the property "time-between-reads" or 2000 millisecs,
-        //default value if the property does not exist in the manifest
-        setPollingWait(POLLING_WAIT); //millisecs interval between hardware device status reads
     }
 
     @Override
     protected void onShowGui() {
-        /**
-         * uncomment the line below to add a GUI to this plugin the GUI can be
-         * started with a right-click on plugin list on the desktop frontend
-         * (com.freedomotic.jfrontend plugin)
-         */
-        //bindGuiToPlugin(new KnxGUI(this));
+        bindGuiToPlugin(settingsMenu);
     }
 
     @Override
     protected void onHideGui() {
-        //implement here what to do when the this plugin GUI is closed
-        //for example you can change the plugin description
+        if(settingsMenu.getParameter(0) != null){
+            KNX_IP = settingsMenu.getParameter(0);
+            KNX_group = settingsMenu.getParameter(1);
+        }
+               
         setDescription("My GUI is now hidden");
     }
 
     @Override
     protected void onRun() {
+        
         try {
-            KNX_value = pc.readBool(new GroupAddress(KNX_group));
-            LOG.info("value read from datapoint " + KNX_group + ": " + KNX_value);
+            if (pc != null) {
+                KNX_value = pc.readBool(new GroupAddress(KNX_group));
+                LOG.info("value read from datapoint " + KNX_group + ": " + KNX_value);
+            }
         } catch (final KNXException e) {
             LOG.info("Error reading KNX datapoint: " + e.getMessage());
         } catch (final InterruptedException e) {
@@ -82,11 +86,102 @@ public class KNX
     @Override
     protected void onStart() {
         LOG.info("KNX plugin is started");
+        
+        settingsMenu = new KNXGui();
+        
+        // inizializzazione GUI
+        /*
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        settingsMenu = new JFrame();
+        settingsMenu.setTitle("KNX-over-IP settings");
+        settingsMenu.setSize(500,300);
+        settingsMenu.setResizable(false);
+        settingsMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel labelPanel = new JPanel(new GridLayout(2, 1));
+        JPanel fieldPanel = new JPanel(new GridLayout(3, 1));
+        
+        final JTextField ipField = new JTextField();
+        ipField.setToolTipText("Inserire IP del bridge KNX/IP");
+        ipField.setColumns(15);
+        JLabel ipLab = new JLabel("KNX IP", JLabel.RIGHT);
+        ipLab.setLabelFor(ipField);
+        labelPanel.add(ipLab);
+        JPanel p = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        p.add(ipField);
+        fieldPanel.add(p);
+        
+        final JTextField groupField = new JTextField();
+        groupField.setToolTipText("Inserire KNX group");
+        groupField.setColumns(5);
+        JLabel groupLab = new JLabel("KNX group", JLabel.RIGHT);
+        groupLab.setLabelFor(groupField);
+        labelPanel.add(groupLab);
+        p = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        p.add(groupField);
+        fieldPanel.add(p);
 
+        
+        final JButton Switch = new JButton("Switch");
+        Switch.setBackground(Color.RED);
+        Switch.setOpaque(true);
+
+        Switch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent a) {
+                try {
+                    if (swState) {
+                        if (pc != null) {
+                            pc.write(new GroupAddress(KNX_group), swState);
+                        }
+                        Switch.setBackground(Color.RED);
+                        swState = false;
+                    } else {
+                        if (pc != null) {
+                            pc.write(new GroupAddress(KNX_group), swState);
+                        }
+                        Switch.setBackground(Color.GREEN);
+                        swState = true;
+                    }
+
+                } catch (final KNXException e) {
+                    LOG.info("Error reading KNX datapoint: " + e.getMessage());
+                }
+            }
+        });
+        labelPanel.add(Switch);
+
+
+        JButton applica = new JButton("Applica");
+
+        applica.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent a) {
+                KNX_IP = ipField.getText();
+                KNX_group = groupField.getText();
+                try {
+                    knxLink = new KNXNetworkLinkIP(KNX_IP, TPSettings.TP1);
+                    pc = new ProcessCommunicatorImpl(knxLink);
+                    LOG.info("read the group value from datapoint " + KNX_group);
+                    final boolean value = pc.readBool(new GroupAddress(KNX_group));
+                    LOG.info("value read from datapoint " + KNX_group + ": " + value);
+                } catch (final KNXException e) {
+                    LOG.info("Error reading KNX datapoint: " + e.getMessage());
+                } catch (final InterruptedException e) {
+                    LOG.info("Interrupted: " + e.getMessage());
+                }
+            }
+        });
+        
+        settingsMenu.add(labelPanel, BorderLayout.WEST);
+        settingsMenu.add(fieldPanel, BorderLayout.CENTER);
+        settingsMenu.add(applica, BorderLayout.SOUTH);
+                
+                */
+            
         try {
             // Create our network link. See other constructors if this one assumes too many
             // default settings.
-            knxLink = new KNXNetworkLinkIP(KNX_IP, TPSettings.TP1);
+            knxLink = new KNXNetworkLinkIP(KNX_IP , TPSettings.TP1);
 
             // create a process communicator using that network link
             pc = new ProcessCommunicatorImpl(knxLink);
@@ -130,7 +225,9 @@ public class KNX
 
     @Override
     protected boolean canExecute(Command c) {
-        //don't mind this method for now
+        
+        // pc.write(group, value);
+        
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
